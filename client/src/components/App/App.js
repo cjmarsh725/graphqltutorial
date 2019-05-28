4734c8456c4deb68ce3ac2c971d1a6c6e0a60d60
Chris <cjmarsh725@gmail.com>
2019-05-28 10:44:18 -0700

    Initial framework

---
+++ client/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React GraphQL GitHub Client</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
